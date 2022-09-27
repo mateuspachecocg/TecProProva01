ed84624c589aeaf353986a2607a77e95eb1ef731
Mateus Pacheco <mateuspacheco@alu.ufc.br>
2022-09-27 14:27:29 -0300

    Implementing Servico class

--- src/salao/Servico.java
+++ src/salao/Servico.java
@@ -1,5 +1,39 @@
 package salao;
 
 public class Servico {
-
+	private int id;
+	private String nome;
+	private double valor;
+	private String descricao;
+	
+	public Servico () {
+		
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getValor() {
+		return valor;
+	}
+	protected void setValor(double valor) {
+		this.valor = valor;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
 }
