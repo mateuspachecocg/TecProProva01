c6c6245350ca83df82950545634806466625299b
Mateus Pacheco <mateuspacheco@alu.ufc.br>
2022-09-27 14:48:24 -0300

    Finish Task 1

--- src/salao/Agendamento.java
+++ src/salao/Agendamento.java
@@ -15,6 +15,42 @@ public class Agendamento {
 		this.cabelereiro = cabelereiro;
 		this.data = Calendar.getInstance();
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getHora() {
+		return hora;
+	}
+
+	public void setHora(int hora) {
+		this.hora = hora;
+	}
+
+	public int getMinuto() {
+		return minuto;
+	}
+
+	public void setMinuto(int minuto) {
+		this.minuto = minuto;
+	}
+
+	public Calendar getData() {
+		return data;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public Cabelereiro getCabelereiro() {
+		return cabelereiro;
+	}
 	
 	
 }
--- src/salao/Main.java
+++ src/salao/Main.java
@@ -3,11 +3,28 @@ package salao;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		// Instanciando clientes
 		Cliente cl1 = new Cliente("61046998390");
 		Cliente cl2 = new Cliente("61046994444");
 		Cliente cl3 = new Cliente("61045555550");
+		// Instanciando cabelereiro
+		Cabelereiro cabelereiro1 = new Cabelereiro("Danilo", "22222222222");
+		Cabelereiro cabelereiro2 = new Cabelereiro("Danilo", "77777777777");
+		Cabelereiro cabelereiro3 = new Cabelereiro("Danilo", "99999999999");
+		// Instanciando Servicos
+		Servico servico1 = new Servico();
+		Servico servico2 = new Servico();
+		Servico servico3 = new Servico();
+		//Instanciando atendimento
+		Atendimento atendimento1 = new Atendimento(cl1);
+		Atendimento atendimento2 = new Atendimento(cl2);
+		Atendimento atendimento3 = new Atendimento(cl3);
+		//Instanciando Agendamento
+		Agendamento agendamento1 = new Agendamento(cl1, cabelereiro1);
+		Agendamento agendamento2 = new Agendamento(cl2, cabelereiro2);
+		Agendamento agendamento3 = new Agendamento(cl3, cabelereiro3);
+	
+	
 	}
 
 }
